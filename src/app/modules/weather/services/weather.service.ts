49a9b09c608c075d0abce9a2529d72ba1943be45
Maurizio Balzano <mau.balzano@gmail.com>
2021-12-13 12:11:18 +0100

    exercise-part-2

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, switchMap } from 'rxjs';
 import { CityWeather, WeatherInfo } from 'src/app/shared/models/weather.model';
 import { environment } from 'src/environments/environment.prod';
 
@@ -12,11 +12,23 @@ export class WeatherService {
   constructor(
     private readonly http: HttpClient
   ) { }
+  getWeatherByCoords(lon:number, lat: number){
+    const params = new HttpParams()
+      .set('appId', environment.apiKey)
+      .set('lat', lat)
+      .set('lon', lon)
+    return this.http.get<any>('https://api.openweathermap.org/data/2.5/onecall', { params })
+  }
+  
+  getCoordByCity(city: string){
+    const params = new HttpParams().set('q', city);
+    return this.http.get<{lon: number, lat: number}>(`https://nominatim.openstreetmap.org/search?q=${city}`)
+      .pipe(
+        switchMap( ({ lon, lat }) => this.getWeatherByCoords(lon, lat) )
+      )
+  }
 
-  // getCoordByCity(city: string){
-  //   const params = new HttpParams().set('q', city);
-  //   return this.http.get('https://nominatim.openstreetmap.org/search', { params })
-  // }
+  
 
   getWeatherByCity(city: string): Observable<WeatherInfo>{
     const params = new HttpParams()
